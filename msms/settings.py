355afb6d0a58da1e6a7fc0ed158b284326241b12
Rahul <rahul.imran@kcl.ac.uk>
2022-11-09 17:07:33 +0000

    Fix: Update settings.py

--- msms/settings.py
+++ msms/settings.py
@@ -11,9 +11,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Heroku settings
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 
 # Quick-start development settings - unsuitable for production
