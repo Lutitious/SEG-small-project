7c33c072835fe41f64694d341cf59fb52e37ff23
Anirudh George <anirudh.george@kcl.ac.uk>
2022-11-18 20:58:35 +0000

    Basic admin for music students

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,8 @@
+"""Configuration of admin interface for lessons"""
 from django.contrib import admin
-
+from .models import MusicStudentUser
 # Register your models here.
+
+class MusicStudentUserAdmin(admin.ModelAdmin):
+    """Configuration of admin interface for music students"""
+    pass
