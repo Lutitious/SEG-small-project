08080e64ab17e00fa3b33f9225cfb517a84b2962
Rahul <rahul.imran@kcl.ac.uk>
2022-11-19 15:07:40 +0000

    Chore: Add additional test cases for username and password

--- lessons/tests/test_log_in.py
+++ lessons/tests/test_log_in.py
@@ -2,6 +2,11 @@ from django.test import TestCase
 from django import forms
 from lessons.forms import LogInForm
 class LogInTest(TestCase):
+
+    def setUp(self):
+        self.form = LogInForm()
+        self.form_input = {'username': 'test', 'password': 'Password1'}
+
     def test_log_in(self):
         response = self.client.get('/log_in/')
         self.assertEqual(response.status_code, 200)
@@ -13,3 +18,17 @@ class LogInTest(TestCase):
         self.assertIn('password', form.fields)
         password_field = form.fields['password']
         self.assertTrue(isinstance(password_field.widget, forms.PasswordInput))
+
+    def test_form_accepts_valid_input(self):
+        form = LogInForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_rejects_no_username(self):
+        self.form_input['username'] = ''
+        form = LogInForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_form_rejects_no_password(self):
+        self.form_input['password'] = ''
+        form = LogInForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
