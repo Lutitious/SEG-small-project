d01934b75ecfa8cce953a01012b60d8632090382
Rahul <rahul.imran@kcl.ac.uk>
2022-11-19 14:48:24 +0000

    Feat: Add a home page containing a link to the log in page

--- lessons/views.py
+++ lessons/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
 def log_in(request):
     return render(request, 'log_in.html')
