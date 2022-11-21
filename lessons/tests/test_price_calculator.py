bd7a30b1dbf697b6717ddfdead1b317a57f3ed48
Mohammed Al Ibrahim <mohammed.1.al_ibrahim@kcl.ac.uk>
2022-11-21 14:11:50 +0000

    Chore: Add additional test cases for price calculation

--- lessons/tests/test_price_calculator.py
+++ lessons/tests/test_price_calculator.py
@@ -3,6 +3,11 @@ from lessons.price_calculator import CalculatePrice
 
 class priceTest(TestCase):
 
+    def setUp(self):
+        CalculatePrice.setCostPerHour(35)
+        CalculatePrice.setPermanentDiscount(1)
+        CalculatePrice.setRentedInstrumentCost(2)
+
     def test_1_hour_lesson(self):
         cost = CalculatePrice(1, False, False).calculatePrice()
         self.assertEqual(cost, 35)
@@ -16,5 +21,14 @@ class priceTest(TestCase):
         self.assertEqual(cost, 26.5)
 
     def test_half_hour_group_lesson(self):
-        cost = CalculatePrice(1, False, True).calculatePrice()
+        cost = CalculatePrice(0.5, False, True).calculatePrice()
         self.assertEqual(cost, 12.25)
+
+    def test_half_hour_group_lesson_cost_30(self):
+        CalculatePrice.setCostPerHour(30)
+        cost = CalculatePrice(0.5, False, True).calculatePrice()
+        self.assertEqual(cost, 10.5)
+
+    def test_half_hour_group_lesson_with_rented_instrument(self):
+        cost = CalculatePrice(0.5, True, True).calculatePrice()
+        self.assertEqual(cost, 13.25)
