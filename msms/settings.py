6ce9e7acaadc8fab83cadbf4b76ba2c40499065b
Rahul Imran <notlutitious@gmail.com>
2022-11-21 16:10:44 +0000

    Chore: Add Migrations

--- msms/settings.py
+++ msms/settings.py
@@ -18,11 +18,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Heroku settings
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "lessons.MusicStudentUser"
